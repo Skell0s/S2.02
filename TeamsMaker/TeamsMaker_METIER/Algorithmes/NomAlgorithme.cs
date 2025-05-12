4be5a68b60868784e0d0e0ff7d8a579448d9b411
BellegueulleMarie <lixy21200@gmail.com>
2025-05-12 10:22:20 +0200

    Base Saé

---
+++ TeamsMaker/TeamsMaker_METIER/Algorithmes/NomAlgorithme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsMaker_METIER.Algorithmes
+{
+    /// <summary>
+    /// Liste des noms d'algorithmes
+    /// </summary>
+    public enum NomAlgorithme
+    {
+        ALGOTEST,
+        GLOUTON_CROISSANT
+    }
+
+
+    public static class NomAlgorithmeExt
+    {
+        /// <summary>
+        /// Affichage du nom de l'algorithme
+        /// </summary>
+        /// <param name="algo">NomAlgorithme</param>
+        /// <returns>La chaine de caractères à afficher</returns>
+        public static string Affichage(this NomAlgorithme algo)
+        {
+            string res = "Algorithme non nommé :(";
+            switch(algo)
+            {
+                case NomAlgorithme.ALGOTEST: res = "Algorithme de test (à supprimer)"; break;
+                case NomAlgorithme.GLOUTON_CROISSANT: res = "Glouton croissant"; break;
+            }
+
+            return res;
+        }
+    }
+}
