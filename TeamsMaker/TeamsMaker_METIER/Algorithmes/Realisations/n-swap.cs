584ce08ae1f32213e9fa8840c08bc99c328fba69
zegemata <julesgras54@gmail.com>
2025-05-16 13:45:24 +0200

    Revert "eztryt"
    This reverts commit 1aa7420d9828982bee6e0c7576c1f6d4eebf621b.

--- TeamsMaker/TeamsMaker_METIER/Algorithmes/Realisations/n-swap.cs
+++ TeamsMaker/TeamsMaker_METIER/Algorithmes/Realisations/n-swap.cs
@@ -13,8 +13,8 @@ namespace TeamsMaker_METIER.Algorithmes.Realisations
     {
         public override Repartition Repartir(JeuTest jeuTest)
         {
-            AlgorithmesProgressif algoInitial = new AlgorithmesProgressif();
-            Repartition repartition = algoInitial.AlgorithmeProgressif(jeuTest);
+            AlgoExtremeEnPremier algoInitial = new AlgoExtremeEnPremier();
+            Repartition repartition = algoInitial.Repartir(jeuTest);
 
             repartition = AppliquerNSwap(repartition, jeuTest);
             repartition = SupprimerEquipeScoreEleve(repartition, jeuTest);
